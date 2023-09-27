755edcf767868de03768a2fa167b7fc20324e378
pgmystery <philipp.glaw@gmx.de>
2023-09-27 22:27:58 +0200

    Frontend: Improve projectlist

--- frontend/src/sites/Projects.tsx
+++ frontend/src/sites/Projects.tsx
@@ -3,46 +3,11 @@ import { Box, Button, Toolbar, Typography } from '@mui/material'
 import AddProjectDialog from '../components/dialog/AddProjectDialog.tsx'
 import SearchInput from '../components/input/SearchInput'
 import ProjectList, { ProjectListProject } from '../components/list/ProjectList/ProjectList.tsx'
-// import { AuthContext } from '../App.tsx'
-// import GitlabFetcher from '../libs/GitlabFetcher.ts'
-// import { useNavigate } from 'react-router-dom'
 
 
 export default function Projects() {
-  // const { state } = useContext(AuthContext)
   const [projects, setProjects] = useState<ProjectListProject[]>([])
   const [open, setOpen] = useState(false)
-
-  // useEffect(() => {
-  //   async function fetchProjects(gitlabFetcher: GitlabFetcher) {
-  //     const result = await gitlabFetcher.getProjects()
-  //
-  //     const projects = result.map(project => {
-  //       return {
-  //         name: project.name,
-  //         nameWithNamespace: project.name_with_namespace,
-  //         avatar: project.avatar_url,
-  //       } as ProjectListProject
-  //     })
-  //
-  //     setProjects(projects)
-  //   }
-  //
-  //   const { user, gitlabURI } = state
-  //
-  //   if (user) {
-  //     const { access_token: accessToken } = user
-  //     const gitlabFetcher = new GitlabFetcher(gitlabURI, accessToken)
-  //
-  //     fetchProjects(gitlabFetcher)
-  //   }
-  //   else {
-  //     navigate('/login', {
-  //       replace: true,
-  //     })
-  //   }
-  //
-  // }, [state, navigate])
 
   const handleClickOpen = () => {
     setOpen(true)
@@ -58,9 +23,6 @@ export default function Projects() {
           project,
         ])
       }
-
-      console.log('ADDING PROJECT')
-      console.log(project)
     }
   }
 
